6546f7438a8275bb327850a93255b3d176758b31
shoile azeez <shoabdulazeez@gmail.com>
2025-03-22 05:35:44 +0100

    update

--- geo_data/views.py
+++ geo_data/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.viewsets  import ModelViewSet
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from user_agents import parse
