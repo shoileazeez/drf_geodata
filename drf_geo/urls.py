8bd2c35e997d5573be2425cc7b684b44379bd369
shoile abdulazeez <shoabdulazeez@gmail.com>
2025-11-16 02:55:32 +0100

    Change API path to root for geo_auth URLs

--- drf_geo/urls.py
+++ drf_geo/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("geo_auth.urls"))
+    path("", include("geo_auth.urls"))
 ]
